e158a891002c97e20ee58ae919d345d9bc02bfe1
ItsMy <mixazpua@hotmail.com>
2019-04-26 12:41:55 +0300

    Updates

---
+++ precache-manifest.f95fac1b56550db448e366e8078c82aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "fada7af6100ba0c470e9",
+    "url": "/reactapp/static/js/main.65997ec2.chunk.js"
+  },
+  {
+    "revision": "4a6ff8217ede826591e6",
+    "url": "/reactapp/static/js/2.a1773a83.chunk.js"
+  },
+  {
+    "revision": "fada7af6100ba0c470e9",
+    "url": "/reactapp/static/css/main.100331ba.chunk.css"
+  },
+  {
+    "revision": "b5738173fcf53e289e01c682bdef9241",
+    "url": "/reactapp/index.html"
+  }
+];
